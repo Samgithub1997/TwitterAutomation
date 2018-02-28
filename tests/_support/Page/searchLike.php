a43130d574e9480494bf42fd47ee6566c0454b8f
Samgithub1997 <samarthsharma351@gmail.com>
2018-02-28 10:37:31 +0530

    GSoC Karna Hai

--- tests/_support/Page/searchLike.php
+++ tests/_support/Page/searchLike.php
@@ -8,7 +8,9 @@ class searchLike
      * You can append any additional parameter to URL
      * and use it in tests like: Page\Edit::route('/123-post');
      */
-    public static $search = ['id' => 'search-query'];
+    public static $url = '/';
+    public static $url1 = '/search?q=chris%20martin&src=typd';
+    public static $searchTweet = ['name' => 'q'];
     
     public static $searchButton = ['xpath' => '/html/body/div[2]/div[1]/div[2]/div/div/div[3]/div/form/span/button'];
 /*    public static function route($param)
@@ -16,5 +18,10 @@ class searchLike
         return static::$URL.$param;
     }
 */
-
+    //comment on the first post
+    public static $comment = ['xpath' => '/html/body/div[2]/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/ol[1]/li[1]/div/div[2]/div[3]/div[2]/div[1]/button'];
+    public static $textarea = ['id' => 'tweet-box-global'];
+    public static $hitReply = ['xpath' => '/html/body/div[23]/div/div[2]/div[4]/form/div[3]/div[2]/button'];
+       
+  
 }
--- tests/acceptance/searchLikeTweetCest.php
+++ tests/acceptance/searchLikeTweetCest.php
@@ -7,16 +7,29 @@ class searchLikeTweetCest
     $I->amOnPage(Page\Login::$url);
     $I->fillField(Page\Login::$username, '7016347478');
     $I->fillField(Page\Login::$password, 'testing123');
-
+    //click on Login button
     $I->click(Page\Login::$loginButton);
 
     //Writing An Automated Test To Search And Like Tweets o.O
-    $I->amOnPage('/');
-    $I->fillField(Page\searchLike::$search,'shahrukh khan');
+    $I->amOnPage(Page\searchLike::$url);
+    $I->fillField(Page\searchLike::$searchTweet,'chris martin');
     $I->click(Page\searchLike::$searchButton);
     
-    
-
-
+    //like buttons 
+    $I->amOnPage(Page\searchLike::$url1);
+    $I->see('chris martin');
+    $I->click(Page\searchLike::$comment);
+    $I->fillField(Page\searchLike::$textarea,'Nice Work');
+    $I->click(Page\searchLike::$hitReply);
+  /*  $I->click(Page\searchLike::$like2);
+    $I->click(Page\searchLike::$like3);
+    $I->click(Page\searchLike::$like4);
+    $I->click(Page\searchLike::$like5);
+    $I->click(Page\searchLike::$like6);
+    $I->click(Page\searchLike::$like7);
+    $I->click(Page\searchLike::$like8);
+    $I->click(Page\searchLike::$like9);
+    $I->click(Page\searchLike::$like10);
+    */
     }
 }
