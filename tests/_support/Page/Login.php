a0fc66533873f91daa3598510ca8cd513006416f
Samarth <Samarthsharma351@gmail.com>
2018-03-10 23:40:24 +0530

    Update Login.php

--- tests/_support/Page/Login.php
+++ tests/_support/Page/Login.php
@@ -6,6 +6,12 @@ class Login
     // include url of current page
     public static $url = '/login';
     
+     //value of username
+    public static $usernameValue = '7016347478';
+
+    //value of password
+    public static $passwordValue = 'testing123';
+    
     //username for logging in
     public static $username = ['class' => 'js-username-field'];
 
